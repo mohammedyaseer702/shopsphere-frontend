785e4dd30bbb441807088b8197475c80005304c0
mohammedyaseer702 <162152845+mohammedyaseer702@users.noreply.github.com>
2026-02-09 13:26:36 +0530

    Add standalone property to register component

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   imports: [FormsModule,CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
